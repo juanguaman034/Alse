f567a5a74c21cdf5e52c343eb484ef81d7291251
juan <juan.guaman-l@mail.escuelaing.edu.co>
2025-09-18 13:58:44 -0500

    Programa de suma con while

--- suma.cpp
+++ suma.cpp
@@ -1,20 +1,31 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main() {
     int a;
     double p;
-    double f;
+    string r="si";
 
-
-    cout << "Ingrese el numero de Articulos" << endl;
-    cin >> a;
-    for (int i=1;i<=a;i++){
-        cout << "Precio del Articulo"<< i << endl;
-        cin >> p;
-        f+=p;
+    while(r=="si"){
+        double f=0.0;
+        cout <<"Desea calcular el costo total de su compra (si/no)"<< endl;
+        cin>> r;
+        if (r=="si"){
+            cout << "Ingrese el numero de Articulos " << endl;
+            cin >> a;
+            for (int i=1;i<=a;i++){
+                cout << "Precio del Articulo "<< i << endl;
+                cin >> p;
+                f+=p;
+            }
+            cout << "El precio final de su compra es de "<< f << endl;
+        }else{
+            cout<<"Tenga un feliz dia"<< endl;
+        }
     }
-    cout << "El precio final de su compra es de "<< f << endl;
+
+
 
 
     return 0;
